477ef8f4e808d27f9ca15b3f984eb2d622bd53c0
nobody <nobody@67f530fe-f72b-0410-b1e4-c16dfad04aa2>
2007-03-14 04:14:52 +0000

    changed key press detection to switch/case for syntactic clarity

--- synth/trunk/sources/SynthWindow.cpp
+++ synth/trunk/sources/SynthWindow.cpp
@@ -25,83 +25,84 @@ void SynthWindow::keyPressEvent( QKeyEvent *event )
 	if( !event->isAutoRepeat() ) {
 
 	int key = event->key();
-	bool got_one = false;
 
-	if( key ==  Qt::Key_A ) {
+	switch (key)
+	{
+		case Qt::Key_A:
 //		cout << " Ab ";
-		got_one = true;
 		ab.add(new WaveForm("A,3,-1", (int)(freq('A',3,-1))));
-	}
-	if( key == Qt::Key_Z ) {
-//		cout << " A ";
-		got_one = true;
-		ab.add(new WaveForm("A,3,0", (int)(freq('A',3,0))));
-	}
-	if( key == Qt::Key_S ) {
-//		cout << " Bb ";
-		got_one = true;
-		ab.add(new WaveForm("B,3,-1", (int)(freq('B',3,-1))));
-	}
-	if( key == Qt::Key_X ) {
-//		cout << " B ";
-		got_one = true;
-		ab.add(new WaveForm("B,3,0", (int)(freq('B',3,0))));
-	}
-	if( key == Qt::Key_C ) {
-//		cout << " C ";
-		got_one = true;
-		ab.add(new WaveForm("C,4,0", (int)(freq('C',4,0))));
-	}
-	if( key == Qt::Key_F ) {
-//		cout << " C# ";
-		got_one = true;
-		ab.add(new WaveForm("C,4,1", (int)(freq('C',4,1))));
-	}
-	if( key == Qt::Key_V ) {
-//		cout << " D ";
-		got_one = true;
-		ab.add(new WaveForm("D,4,0", (int)(freq('D',4,0))));
-	}
-	if( key == Qt::Key_G ) {
-//		cout << " D# ";
-		got_one = true;
-		ab.add(new WaveForm("D,4,1", (int)(freq('D',4,1))));
-	}
-	if( key == Qt::Key_B ) {
-//		cout << " E ";
-		got_one = true;
-		ab.add(new WaveForm("E,4,0", (int)(freq('E',4,0))));
-	}
-	if( key == Qt::Key_N ) {
-//		cout << " F ";
-		got_one = true;
-		ab.add(new WaveForm("F,4,0", (int)(freq('F',4,0))));
-	}
-	if( key == Qt::Key_J ) {
-//		cout << " F# ";
-		got_one = true;
-		ab.add(new WaveForm("F,4,1", (int)(freq('F',4,1))));
-	}
-	if( key == Qt::Key_M ) {
-//		cout << " G ";
-		got_one = true;
-		ab.add(new WaveForm("G,4,0", (int)(freq('G',4,0))));
-	}
-	if( key == Qt::Key_K ) {
-//		cout << " G# ";
-		got_one = true;
-		ab.add(new WaveForm("G,4,1", (int)(freq('G',4,1))));
-	}
-	if( key == Qt::Key_Comma ) {
-//		cout << " A ";
-		got_one = true;
-		ab.add(new WaveForm("A,4,0", (int)(freq('A',4,0))));
-	}
-	
+		break;
 
-	cout.flush();
-	if( !got_one )
+		case Qt::Key_Z:
+//		cout << " A ";
+		ab.add(new WaveForm("A,3,0", (int)(freq('A',3,0))));
+		break;
+
+		case Qt::Key_S:
+//		cout << " Bb ";
+		ab.add(new WaveForm("B,3,-1", (int)(freq('B',3,-1))));
+		break;
+
+		case Qt::Key_X:
+//		cout << " B ";
+		ab.add(new WaveForm("B,3,0", (int)(freq('B',3,0))));
+		break;
+
+		case Qt::Key_C:
+//		cout << " C ";
+		ab.add(new WaveForm("C,4,0", (int)(freq('C',4,0))));
+		break;
+
+		case Qt::Key_F:
+//		cout << " C# ";
+		ab.add(new WaveForm("C,4,1", (int)(freq('C',4,1))));
+		break;
+
+		case Qt::Key_V:
+//		cout << " D ";
+		ab.add(new WaveForm("D,4,0", (int)(freq('D',4,0))));
+		break;
+
+		case Qt::Key_G:
+//		cout << " D# ";
+		ab.add(new WaveForm("D,4,1", (int)(freq('D',4,1))));
+		break;
+
+		case Qt::Key_B:
+//		cout << " E ";
+		ab.add(new WaveForm("E,4,0", (int)(freq('E',4,0))));
+		break;
+
+		case Qt::Key_N:
+//		cout << " F ";
+		ab.add(new WaveForm("F,4,0", (int)(freq('F',4,0))));
+		break;
+
+		case Qt::Key_J:
+//		cout << " F# ";
+		ab.add(new WaveForm("F,4,1", (int)(freq('F',4,1))));
+		break;
+
+		case Qt::Key_M:
+//		cout << " G ";
+		ab.add(new WaveForm("G,4,0", (int)(freq('G',4,0))));
+		break;
+
+		case Qt::Key_K:
+//		cout << " G# ";
+		ab.add(new WaveForm("G,4,1", (int)(freq('G',4,1))));
+		break;
+
+		case Qt::Key_Comma:
+//		cout << " A ";
+		ab.add(new WaveForm("A,4,0", (int)(freq('A',4,0))));
+		break;
+		
+		default:
 		QWidget::keyPressEvent(event);
+
+	}
+//	cout.flush();
 
 	}
 }
@@ -109,85 +110,86 @@ void SynthWindow::keyPressEvent( QKeyEvent *event )
 void SynthWindow::keyReleaseEvent( QKeyEvent *event )
 {
 	int key = event->key();
-	bool got_one = false;
 
 	if( !event->isAutoRepeat() ) {
 	//	cout << "key release" << endl;
 
-	if( key ==  Qt::Key_A ) {
+	switch ( key )
+	{
+		case Qt::Key_A:
 //		cout << " Ab ";
-		got_one = true;
 		ab.remove("A,3,-1");
-	}
-	if( key == Qt::Key_Z ) {
+		break;
+
+		case Qt::Key_Z:
 //		cout << " A ";
-		got_one = true;
 		ab.remove("A,3,0");
-		//currentNotes.remove("A,3,0");
-	}
-	if( key == Qt::Key_S ) {
+		break;
+
+		case Qt::Key_S:
 //		cout << " Bb ";
-		got_one = true;
 		ab.remove("B,3,-1");
-	}
-	if( key == Qt::Key_X ) {
+		break;
+
+		case Qt::Key_X:
 //		cout << " B ";
-		got_one = true;
 		ab.remove("B,3,0");
-	}
-	if( key == Qt::Key_C ) {
+		break;
+
+		case Qt::Key_C:
 //		cout << " C ";
-		got_one = true;
 		ab.remove("C,4,0");
-	}
-	if( key == Qt::Key_F ) {
+		break;
+
+		case Qt::Key_F:
 //		cout << " C# ";
-		got_one = true;
 		ab.remove("C,4,1");
-	}
-	if( key == Qt::Key_V ) {
+		break;
+
+		case Qt::Key_V:
 //		cout << " D ";
-		got_one = true;
 		ab.remove("D,4,0");
-	}
-	if( key == Qt::Key_G ) {
+		break;
+
+		case Qt::Key_G:
 //		cout << " D# ";
-		got_one = true;
 		ab.remove("D,4,1");
-	}
-	if( key == Qt::Key_B ) {
+		break;
+
+		case Qt::Key_B:
 //		cout << " E ";
-		got_one = true;
 		ab.remove("E,4,0");
-	}
-	if( key == Qt::Key_N ) {
+		break;
+
+		case Qt::Key_N:
 //		cout << " F ";
-		got_one = true;
 		ab.remove("F,4,0");
-	}
-	if( key == Qt::Key_J ) {
+		break;
+
+		case Qt::Key_J:
 //		cout << " F# ";
-		got_one = true;
 		ab.remove("F,4,1");
-	}
-	if( key == Qt::Key_M ) {
+		break;
+
+		case Qt::Key_M:
 //		cout << " G ";
-		got_one = true;
 		ab.remove("G,4,0");
-	}
-	if( key == Qt::Key_K ) {
+		break;
+
+		case Qt::Key_K:
 //		cout << " G# ";
-		got_one = true;
 		ab.remove("G,4,1");
-	}
-	if( key == Qt::Key_Comma ) {
+		break;
+
+		case Qt::Key_Comma:
 //		cout << " A ";
-		got_one = true;
 		ab.remove("A,4,0");
-	}
-	cout.flush();
-	if( !got_one )
+		break;
+
+		default:
 		QWidget::keyPressEvent(event);
+	}
+//	cout.flush();
 
 	}
 }
